b3106734cb6227f007da26a19a8d37b349913ff4
Angelo Russoniello <angelorussoniello88gmail.com>
2018-01-29 16:49:17 -0800

    Adds logic interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //business logic
 var items = [];
-items.
+
 
 //User Interface
 $(document).ready(function() {
   $("#submitButton").click(function() {
       $("#groceryForm").hide();
+      var result = $("#items-input").val();
 
+
+      $(".result").text(result);
   });
 });
