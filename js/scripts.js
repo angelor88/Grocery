801db0642f1a62e774ad974421e4261a38244ab8
Angelo Russoniello <angelorussoniello88gmail.com>
2018-01-29 16:28:22 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//business logic
+var items = [];
+items.
+
+//User Interface
+$(document).ready(function() {
+  $("#submitButton").click(function() {
+      $("#groceryForm").hide();
+
+  });
+});
